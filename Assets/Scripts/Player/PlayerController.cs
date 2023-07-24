770157667800617d41865d14b9ff3a09d880a40d
Zakhar Vislov <tabak1805@gmail.com>
2023-07-25 00:15:53 +0300

    WIP gravity fix

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,37 +7,68 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private float _speed;
+    [SerializeField]
+    private float _cameraSensetivitySpeed;
+    [SerializeField]
+    private float _gravity = -9.8f;
 
     private CharacterController _characterController;
+    private Vector2 _cameraRotation;
+
+    
 
     public InputAction moveAction;
+    public InputAction lookAction;
 
     private void Start()
     {
         _characterController = GetComponent<CharacterController>();
+        //_rb = GetComponent<Rigidbody>();
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        //if (_rb != null)
+        //    _rb.freezeRotation = true;
     }
 
     private void Update()
     {
+        // Update orientation first, then move. Otherwise move orientation will lag
+        // behind by one frame.
+        Look(lookAction.ReadValue<Vector2>());
         Move(moveAction.ReadValue<Vector3>());
     }
 
     private void OnEnable()
     {
+        lookAction.Enable();
         moveAction.Enable();
     }
 
     private void OnDisable()
     {
         moveAction.Disable();
+        lookAction.Disable();
     }
 
     private void Move(Vector3 direction)
     {
         var scaledSpeed = _speed * Time.deltaTime;
-        var newDirection = new Vector3(direction.x * scaledSpeed, direction.y * scaledSpeed, direction.z * scaledSpeed);
+        var newDirection = new Vector3(direction.x, 0, direction.z);
+        newDirection = Vector3.ClampMagnitude(newDirection, scaledSpeed);
+        //newDirection.y = _gravity * Time.deltaTime;
+        _characterController.Move(_characterController.transform.TransformDirection(newDirection));
+    }
 
-        Debug.Log($"newDir: {newDirection}");
-        _characterController.Move(newDirection);
+    private void Look(Vector2 rotation)
+    {
+        if (rotation.sqrMagnitude < 0.01)
+            return;
+        var scaledRotateSpeed = _cameraSensetivitySpeed * Time.deltaTime;
+        _cameraRotation.y += rotation.x * scaledRotateSpeed;
+        _cameraRotation.x = Mathf.Clamp(_cameraRotation.x - rotation.y * scaledRotateSpeed, -70, 70);
+
+        transform.localEulerAngles = _cameraRotation;
     }
 }
