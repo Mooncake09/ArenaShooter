45727edbde7c34c9d07d059bb9a385980e105507
Zakhar Vislov <tabak1805@gmail.com>
2023-07-30 13:29:23 +0300

    wip

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -10,28 +10,22 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private float _jumpForce;
     [SerializeField]
-    private float _gravityMultiplier;
+    private float _gravityMultiplier = 1f;
 
     private CharacterController _characterController;
     private Rigidbody _rigidbody;
     private Vector2 _cameraRotation;
 
-    private const float GRAVITY = -9.81f;
-    [SerializeField]
-    private float _fallVelocity = 3.0f;
+    private float _mouseXInput;
+    private float _mouseYInput;
+
     private Vector3 _direction;
 
-
-    public InputAction moveAction;
-    public InputAction lookAction;
-    public InputAction jumpAction;
+    //public InputAction lookAction;
 
     private void Start()
     {
         _characterController = GetComponent<CharacterController>();
-        _rigidbody = GetComponent<Rigidbody>();
-
-        _direction = transform.position;
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
@@ -41,46 +35,18 @@ public class PlayerController : MonoBehaviour
     {
         // Update orientation first, then move. Otherwise move orientation will lag
         // behind by one frame.
-        ApplyGravity();
-        Look(lookAction.ReadValue<Vector2>());
-        Move(moveAction.ReadValue<Vector3>());
-    }
-
-    private void OnEnable()
-    {
-        lookAction.Enable();
-        moveAction.Enable();
-        jumpAction.Enable();
-    }
-
-    private void OnDisable()
-    {
-        moveAction.Disable();
-        lookAction.Disable();
-        jumpAction.Disable();
-    }
-
-    private void Move(Vector3 direction)
-    {
-        if (direction.sqrMagnitude < 0.01)
-            return;
-        var scaledMoveSpeed = _speed * Time.deltaTime;
-        // For simplicity's sake, we just keep movement in a single plane here. Rotate
-        // direction according to world Y rotation of player.
-        _direction = Quaternion.Euler(0, transform.eulerAngles.y, 0) * new Vector3(direction.x, 0, direction.z);
-
-        //if (!_characterController.isGrounded)
-        //    direction += Physics.gravity;
-
-        _direction = Vector3.ClampMagnitude(_direction, _speed) * scaledMoveSpeed;
-
-        _characterController.Move(_direction);
+        //Look(lookAction.ReadValue<Vector2>());
+        ApplyMove();
+        Look(new Vector2(_mouseXInput, _mouseYInput));
+        
     }
 
     private void Look(Vector2 rotation)
     {
+        Debug.Log(rotation);
         if (rotation.sqrMagnitude < 0.01)
             return;
+
         var scaledRotateSpeed = _cameraSensetivitySpeed * Time.deltaTime;
         _cameraRotation.y += rotation.x * scaledRotateSpeed;
         _cameraRotation.x = Mathf.Clamp(_cameraRotation.x - rotation.y * scaledRotateSpeed, -70, 70);
@@ -88,13 +54,25 @@ public class PlayerController : MonoBehaviour
         transform.localEulerAngles = _cameraRotation;
     }
 
-    private void ApplyGravity()
+    public void Move(InputAction.CallbackContext context)
     {
-        if (_characterController.isGrounded && _fallVelocity < 0.0f)
-            _fallVelocity = -1.0f;
-        else
-            _fallVelocity += GRAVITY * _gravityMultiplier * Time.deltaTime;
+        var input = context.ReadValue<Vector2>();
 
-        _characterController.Move(new Vector3(0, _fallVelocity, 0));
+        var movement = new Vector3(input.x, 0, input.y) * _speed;
+        movement = Vector3.ClampMagnitude(movement, _speed);
+        movement += Physics.gravity * _gravityMultiplier;
+        movement *= Time.deltaTime;
+        movement = transform.TransformDirection(movement);
+
+        _direction = movement;
     }
+
+    public void MouseX(InputAction.CallbackContext context) => _mouseXInput = context.ReadValue<float>();
+    public void MouseY(InputAction.CallbackContext context) => _mouseYInput = context.ReadValue<float>();
+
+
+    private void ApplyMove() 
+    {
+        _characterController.Move(_direction);
+    } 
 }
